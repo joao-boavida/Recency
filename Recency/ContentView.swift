aa03b384063e7a343bade768198eeb328c8811df
João Boavida <j.m.boavida@gmail.com>
2020-10-12 11:28:19 +0100

    Main screen (Content View) basic layout completed

--- Recency/ContentView.swift
+++ Recency/ContentView.swift
@@ -7,10 +7,49 @@
 
 import SwiftUI
 
+struct FlightActivity: Identifiable, Codable {
+    var id = UUID()
+    var insertionDate = Date()
+    let takeoffs: Int
+    let takeoffDate: Date
+    let landings: Int
+    let landingDates: Date
+}
+
+class FlightLog: ObservableObject {
+
+    let storageKey = "FlightActivity"
+
+    @Published var data = [FlightActivity]()
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Section {
+                    Text("Current Status Here")
+                        .font(.largeTitle)
+                    Text("Next Limitation")
+                        .font(.headline)
+                }
+                Form {
+                    Section {
+                        Text("Takeoffs and date")
+                        Text("Landings and date")
+                    }
+                    Button("See activities...") {
+                        // present sheet to see activities
+                    }
+                    Button("Add Activity...") {
+                    //Present sheet to add activity
+                    }
+
+                }
+            }
+            .navigationBarTitle("Recency Monitor")
+        }
+
     }
 }
 
