caab1beb2975ff14b6b7952a7fb67ffef8bc689d
João Boavida <j.m.boavida@gmail.com>
2020-10-12 09:13:47 +0100

    Initial Commit

---
+++ Recency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recency
+//
+//  Created by Joao Boavida on 12/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recency/RecencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecencyApp.swift
+//  Recency
+//
+//  Created by Joao Boavida on 12/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct RecencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
