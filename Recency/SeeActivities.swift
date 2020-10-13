2eb00108df4708804a4310ca731ed5790dab19ed
João Boavida <j.m.boavida@gmail.com>
2020-10-13 12:30:03 +0100

    swipe to delete implemented at the activity screen, working with a glitch: the sheet disappears and then reappears again.

--- Recency/SeeActivities.swift
+++ Recency/SeeActivities.swift
@@ -13,14 +13,17 @@ struct SeeActivities: View {
 
     @Environment(\.presentationMode) var presentationMode
 
+
     var body: some View {
         NavigationView {
-            List { // temporary for dev
+            Form {
                 ForEach(flightLog.data) { activity in
                     Text("\(activity.takeoffDate.description)")
                     Text("\(activity.takeoffs)")
                 }
+                .onDelete(perform: removeItems)
             }
+
             .navigationBarTitle("Flight Activities")
             .navigationBarItems(trailing: Button("Close") {
                 presentationMode.wrappedValue.dismiss()
@@ -28,10 +31,51 @@ struct SeeActivities: View {
         }
 
     }
+
+    func removeItems(at offsets: IndexSet) {
+        flightLog.data.remove(atOffsets: offsets)
+    }
 }
 
 struct SeeActivities_Previews: PreviewProvider {
     static var previews: some View {
-        SeeActivities(flightLog: FlightLog())
+
+        let sampleFlightLog = FlightLog()
+
+
+        // referenceDate
+
+        var components = DateComponents()
+        components.year = 2020
+        components.month = 10
+        components.day = 13
+        //components.calendar = .current
+
+        let referenceDate = Calendar.current.date(from: components)!
+
+        let movement1 = FlightActivity(takeoffs: 1, takeoffDate: referenceDate, landings: 1, landingDate: referenceDate)
+
+        // 3 days ago
+        let threeDaysAgo = Calendar.current.date(byAdding: .day, value: -3, to: referenceDate)!
+
+        let movement2 = FlightActivity(takeoffs: 1, takeoffDate: threeDaysAgo, landings: 1, landingDate: threeDaysAgo)
+
+        // 30 days ago
+        let thirtyDaysAgo = Calendar.current.date(byAdding: .day, value: -30, to: referenceDate)!
+
+        let movement3 = FlightActivity(takeoffs: 1, takeoffDate: thirtyDaysAgo, landings: 1, landingDate: thirtyDaysAgo)
+
+        // 100 days ago
+        let hundredDaysAgo = Calendar.current.date(byAdding: .day, value: -100, to: referenceDate)!
+
+        let movement4 = FlightActivity(takeoffs: 1, takeoffDate: hundredDaysAgo, landings: 1, landingDate: hundredDaysAgo)
+
+
+        sampleFlightLog.data.append(movement1)
+        sampleFlightLog.data.append(movement2)
+        sampleFlightLog.data.append(movement3)
+        sampleFlightLog.data.append(movement4)
+
+        return SeeActivities(flightLog: sampleFlightLog)
     }
 }
